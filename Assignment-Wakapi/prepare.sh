4f10b5da4d3adc2beb949d73f6342365da4ff5b0
Phuwit Puthipairoj <26784267+phuwit@users.noreply.github.com>
2026-02-21 18:00:00 +0700

    update gitignores and change prepare.sh to use apt-get

--- Assignment-Wakapi/prepare.sh
+++ Assignment-Wakapi/prepare.sh
@@ -2,19 +2,19 @@
 
 
 # update and upgrade
-sudo apt update
-sudo apt upgrade -y
+apt-get update
+apt-get upgrade -y
 
 
 # install docker
 # Add Docker's official GPG key:
-sudo apt install ca-certificates curl
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
+apt-get install ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
 
 # Add the repository to Apt sources:
-sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+tee /etc/apt/sources.list.d/docker.sources <<EOF
 Types: deb
 URIs: https://download.docker.com/linux/debian
 Suites: $(. /etc/os-release && echo "$VERSION_CODENAME")
@@ -22,11 +22,11 @@ Components: stable
 Signed-By: /etc/apt/keyrings/docker.asc
 EOF
 
-sudo apt update
+apt-get update
 
 # install docker for real
-sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 
 # run compose
-sudo docker compose up -d
+docker compose up -d
