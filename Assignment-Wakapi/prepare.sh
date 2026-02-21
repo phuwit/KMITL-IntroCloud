a5cc29722207565b5936026e39e3eeb67f604b8c
Phuwit Puthipairoj <26784267+phuwit@users.noreply.github.com>
2026-02-21 18:08:32 +0700

    add a -y flag for apt-get

--- Assignment-Wakapi/prepare.sh
+++ Assignment-Wakapi/prepare.sh
@@ -8,7 +8,7 @@ apt-get upgrade -y
 
 # install docker
 # Add Docker's official GPG key:
-apt-get install ca-certificates curl
+apt-get install -y ca-certificates curl
 install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 chmod a+r /etc/apt/keyrings/docker.asc
@@ -25,7 +25,7 @@ EOF
 apt-get update
 
 # install docker for real
-apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 
 # run compose
