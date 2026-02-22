b012e14d9897da23d2ca41b8e3f95102d385a5eb
Phuwit Puthipairoj <26784267+phuwit@users.noreply.github.com>
2026-02-22 19:06:24 +0700

    test one probably works now

--- Assignment-Wakapi/startup-wakapi.sh
+++ Assignment-Wakapi/startup-wakapi.sh
@@ -36,22 +36,48 @@ services:
   wakapi:
     image: ghcr.io/muety/wakapi:latest
     ports:
-      - 3000:3000
+      - 80:3000
     restart: unless-stopped
     volumes:
       - ./wakapi-data:/data
+    secrets:
+      - source: password_salt
+        target: password_salt
+        uid: '1000'
+        gid: '1000'
+        mode: '0400'
+
+      - source: db_password
+        target: db_password
+        uid: '1000'
+        gid: '1000'
+        mode: '0400'
+    environment:
+      - WAKAPI_DB_TYPE
+      - WAKAPI_DB_NAME
+      - WAKAPI_DB_USER
+      - WAKAPI_DB_HOST
+      - WAKAPI_DB_PORT
+      - WAKAPI_DB_PASSWORD
+      - WAKAPI_PASSWORD_SALT
+
+secrets:
+  password_salt:
+    environment: WAKAPI_PASSWORD_SALT
+  smtp_pass:
+    environment: WAKAPI_MAIL_SMTP_PASS
+  db_password:
+    environment: WAKAPI_DB_PASSWORD
 EOF
 
-# write variables to a file (it wouldnt wouldnt with export for some reason)
-tee "$APP_DIRECTORY/.env" <<EOF
-WAKAPI_DB_TYPE="$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/WAKAPI_DB_TYPE -H "Metadata-Flavor: Google")"
-WAKAPI_DB_NAME="$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/WAKAPI_DB_NAME -H "Metadata-Flavor: Google")"
-WAKAPI_DB_USER="$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/WAKAPI_DB_USER -H "Metadata-Flavor: Google")"
-WAKAPI_DB_HOST="$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/WAKAPI_DB_HOST -H "Metadata-Flavor: Google")"
-WAKAPI_DB_PORT="$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/WAKAPI_DB_PORT -H "Metadata-Flavor: Google")"
-WAKAPI_DB_PASSWORD="$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/WAKAPI_DB_PASSWORD -H "Metadata-Flavor: Google")"
-WAKAPI_PASSWORD_SALT="$(cat /dev/urandom | LC_ALL=C tr -dc 'a-zA-Z0-9' | fold -w ${1:-32} | head -n 1)"
-EOF
+# export variables
+export WAKAPI_DB_TYPE=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/WAKAPI_DB_TYPE -H "Metadata-Flavor: Google")
+export WAKAPI_DB_NAME=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/WAKAPI_DB_NAME -H "Metadata-Flavor: Google")
+export WAKAPI_DB_USER=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/WAKAPI_DB_USER -H "Metadata-Flavor: Google")
+export WAKAPI_DB_HOST=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/WAKAPI_DB_HOST -H "Metadata-Flavor: Google")
+export WAKAPI_DB_PORT=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/WAKAPI_DB_PORT -H "Metadata-Flavor: Google")
+export WAKAPI_DB_PASSWORD="$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/WAKAPI_DB_PASSWORD -H "Metadata-Flavor: Google")"
+export WAKAPI_PASSWORD_SALT="$(cat /dev/urandom | LC_ALL=C tr -dc 'a-zA-Z0-9' | fold -w ${1:-32} | head -n 1)"
 
 # run compose
-docker compose up -d
+docker compose --file "$APP_DIRECTORY/compose.yml" up --detach
